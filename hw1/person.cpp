14dd7bd2a723be6d28fc40720e77f9bbd7d11dc6
zachmichael14 <zachmichael14@gmail.com>
2023-09-06 11:46:11 -0500

    Change ctor and method signatures to match spec

--- hw1/person.cpp
+++ hw1/person.cpp
@@ -7,7 +7,7 @@
 
 // Constructor
 Person::Person() {}
-Person::Person(std::string name, std::string email, std::string phone) : _name(name), _email(email), _phone(phone) {}
+Person::Person(std::string _name, std::string _email, std::string _phone) : _name(_name), _email(_email), _phone(_phone) {}
 
 // Get name
 std::string Person::name() const {
@@ -15,9 +15,9 @@ std::string Person::name() const {
 }
 
 // Set name
-void Person::name(std::string& name) 
+void Person::name(std::string& _name) 
 {
-  this->_name = name;
+  this->_name = _name;
 }
 
 // Get email
@@ -26,8 +26,8 @@ std::string Person::email() const {
 }
 
 // Set email
-void Person::email(std::string& email) {
-  this->_email = email;
+void Person::email(std::string& _email) {
+  this->_email = _email;
 }
 
 // Get phone
@@ -36,6 +36,6 @@ std::string Person::phone() const {
 }
 
 // Set phone
-void Person::phone(std::string& phone) {
-  this->_phone = phone;
+void Person::phone(std::string& _phone) {
+  this->_phone = _phone;
 }
