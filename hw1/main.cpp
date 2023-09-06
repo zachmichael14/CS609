9ebb4781d0dafa02222ee75f94532dc0af27f16c
zachmichael14 <zachmichael14@gmail.com>
2023-09-06 12:10:53 -0500

    Test employee implementation

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -4,12 +4,16 @@
 #include "person.h"
 #include "purchase.h"
 #include "customer.h"
+#include "employee.h"
 
 
 
 int main() {
-    //////// TEST FOR USER-CREATED CUSTOMER AND PURCHASE //////
+
+
+    /////// TEST FOR USER-CREATED EMPLOYEE //////
     char choice;
+
     std::cout << "Choice, A or M?";
     std::cin >> choice;
 
@@ -17,6 +21,7 @@ int main() {
         std::string name;
         std::string email;
         std::string phone;
+        double salary;
         
         std::cout << "Name: ";
         std:: cin >> name;
@@ -27,49 +32,82 @@ int main() {
         std::cout << "Phone: ";
         std:: cin >> phone;
 
-        Customer c1(name, email, phone);
+        // std::cout << "Salary: ";
+        // std::cin >> salary;
 
-        std::cout << "Customer " << c1.name() << " created." << std::endl;
-        std::cout << c1.email() << c1.phone() << std::endl;
+        // Employee e1(name, email, phone, salary);
+        Employee e1(name, email, phone, 50000.25);
 
-        std::cout << "Add purchase" << std::endl;
-
-        std::string item;
-        int qty;
-        double cost;
-
-        std::cout << "Item: ";
-        std:: cin >> item;
-
-        std::cout << "Quantity: ";
-        std:: cin >> qty;
-
-        std::cout << "Cost: ";
-        std:: cin >> cost;
-
-        Purchase p1(item, qty, cost);
-
-        std::cout << "Adding " << p1.item() << " to customer " << c1.name() << " history" << std::endl;
-
-        c1.add_purchase(p1);
-        // Purchase p1(item, qty, cost);
-
-        c1.add_purchase(Purchase("Gadget", 4, 4.6));
-
-
-        // Use the iterators to iterate through the purchases
-        std::vector<Purchase>::const_iterator it = c1.purchase_begin();
-        std::vector<Purchase>::const_iterator end = c1.purchase_end();
-    
-        while (it != end) {
-            const Purchase& purchase = *it;
-            // Access purchase data or perform operations
-            std::cout << "Item: " << purchase.item() << ", Quantity: " << purchase.qty() << ", Cost: " << purchase.cost() << std::endl;
-        
-            ++it; // Move to the next purchase
-        }
-
+        std::cout << "Employee " << e1.name() << " created." << std::endl;
+        std::cout << e1.email() << e1.phone() << std::endl;
+        std::cout << e1.salary() << std::endl;
     }
+    ////// END EMPLOYEE TEST ///////
+
+
+
+    //////// TEST FOR USER-CREATED CUSTOMER AND PURCHASE //////
+    // char choice;
+    // std::cout << "Choice, A or M?";
+    // std::cin >> choice;
+
+    // if (choice == 'A') {
+    //     std::string name;
+    //     std::string email;
+    //     std::string phone;
+        
+    //     std::cout << "Name: ";
+    //     std:: cin >> name;
+
+    //     std::cout << "Email: ";
+    //     std:: cin >> email;
+
+    //     std::cout << "Phone: ";
+    //     std:: cin >> phone;
+
+    //     Customer c1(name, email, phone);
+
+    //     std::cout << "Customer " << c1.name() << " created." << std::endl;
+    //     std::cout << c1.email() << c1.phone() << std::endl;
+
+    //     std::cout << "Add purchase" << std::endl;
+
+    //     std::string item;
+    //     int qty;
+    //     double cost;
+
+    //     std::cout << "Item: ";
+    //     std:: cin >> item;
+
+    //     std::cout << "Quantity: ";
+    //     std:: cin >> qty;
+
+    //     std::cout << "Cost: ";
+    //     std:: cin >> cost;
+
+    //     Purchase p1(item, qty, cost);
+
+    //     std::cout << "Adding " << p1.item() << " to customer " << c1.name() << " history" << std::endl;
+
+    //     c1.add_purchase(p1);
+    //     // Purchase p1(item, qty, cost);
+
+    //     c1.add_purchase(Purchase("Gadget", 4, 4.6));
+
+
+    //     // Use the iterators to iterate through the purchases
+    //     std::vector<Purchase>::const_iterator it = c1.purchase_begin();
+    //     std::vector<Purchase>::const_iterator end = c1.purchase_end();
+    
+    //     while (it != end) {
+    //         const Purchase& purchase = *it;
+    //         // Access purchase data or perform operations
+    //         std::cout << "Item: " << purchase.item() << ", Quantity: " << purchase.qty() << ", Cost: " << purchase.cost() << std::endl;
+        
+    //         ++it; // Move to the next purchase
+    //     }
+
+    // }
     
     ////// END CUSTOMER AND PURCHASE TEST ///////
 
