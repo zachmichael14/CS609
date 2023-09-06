4fddbccaae787aa2efbaa9185cbdd28ee708ee00
zachmichael14 <zachmichael14@gmail.com>
2023-09-06 11:22:14 -0500

    Finish iterator methods

--- hw1/customer.cpp
+++ hw1/customer.cpp
@@ -2,15 +2,33 @@
 // File: customer.cpp
 // Purpose: Implemenation of the customer class.
 ///////////////////////////////////////////////////////////////////////////
+#include <iostream>
 #include <string>
 #include "customer.h"
+#include "person.h"
+#include "purchase.h"
 
-Customer::Customer() {}
+// Initialize _history vector ???
+Customer::Customer() : Person()
+{
+  _history = new std::vector<Purchase>();
+}
 
-Customer::Customer(std::string name, std::string email, std::string phone) : _name(name) _email(email), _phone(phone) {}
+Customer::Customer(std::string name, std::string email, std::string phone) : Person::Person(name, email, phone)
+{
+  _history = new std::vector<Purchase>();
+}
 
-// Pass by reference since the add_purchase doesn't modify the purchase object 
-// and no need to make a copy of the object??
+//??? Pass by reference since the add_purchase doesn't modify the purchase object 
+//??? and no need to make a copy of the object??
 void Customer::add_purchase(const Purchase& purchase) {
-  _history.push_back(purchase)
+  _history->push_back(purchase);
+}
+
+std::vector<Purchase>::const_iterator Customer::purchase_begin() {
+  return _history->begin();
+}
+
+std::vector<Purchase>::const_iterator Customer::purchase_end() {
+  return _history->end();
 }
