11def53e254bc6ccfab6d35bb8b744cd138958cb
zachmichael14 <zachmichael14@gmail.com>
2023-09-06 09:54:59 -0500

    Add customer definition

--- hw1/customer.h
+++ hw1/customer.h
@@ -4,5 +4,21 @@
 ///////////////////////////////////////////////////////////////////////////
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
+#include <vector>
+#include "purchase.h"
 
+class Customer : public Person
+{
+public:
+  // Constructor
+  Customer();
+  Customer(std::string _name, std::string _email, std::string _phone);
+
+  void add_purchase(Purchase purchase);
+  std::vector<Purchase>::const_iterator purchase_begin();
+  std::vector<Purchase>::const_iterator purchase_end();
+
+private:
+  std::vector<Purchase> *_history;
+};
 #endif
