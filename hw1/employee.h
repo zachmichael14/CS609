716103b487a8a5aa541b2475f93ca444340c9ef9
zachmichael14 <zachmichael14@gmail.com>
2023-09-05 23:24:40 -0500

    Add employee class definition

--- hw1/employee.h
+++ hw1/employee.h
@@ -1,8 +1,25 @@
 ///////////////////////////////////////////////////////////////////////////
 // File: employee.h
-// Purpose: TBD.
+// Purpose: Class definition of an employee.
 ///////////////////////////////////////////////////////////////////////////
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
+#include "person.h"
 
+class Employee : public Person 
+{
+public:
+  // Default constructor
+  Employee() {};
+
+  // Parameterized constructor
+  Employee(std::string _name, std:string _email, std::string _phone, double _salary) {};
+
+  double salary() const;
+  void salary(double salary);
+
+private:
+  double _salary;
+
+};
 #endif
