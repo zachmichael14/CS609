b4bac75f437190ae9bcad16b18e0c7865eede344
zachmichael14 <zachmichael14@gmail.com>
2023-09-05 20:17:48 -0500

    Add constructors, name methods

--- hw1/person.h
+++ hw1/person.h
@@ -2,3 +2,36 @@
 // File: person.h
 // Purpose: TBD.
 ///////////////////////////////////////////////////////////////////////////
+
+// This will be a pure abstract class from which the concrete
+// classes Employee and Customer will be dervied
+
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+
+class Person
+{
+public:
+
+  // Default constructor
+  Person() {};
+
+  // Parameterized constructor
+  Person(std::string name, std::string email, std::string phone);
+
+  std::string name() const;
+  void name(std::string _name);
+
+
+  // TODO
+
+private:
+  std::string _name;
+  std::string _email
+  std::string _phone;
+
+}
+
+#endif // PERSON_H
