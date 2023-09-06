47f35246e7a7ce52c2783c7b79693594f2d5bb61
zachmichael14 <zachmichael14@gmail.com>
2023-09-05 22:00:32 -0500

    Add member variable setters/getters

--- hw1/person.cpp
+++ hw1/person.cpp
@@ -1,15 +1,41 @@
+///////////////////////////////////////////////////////////////////////////
+// File: person.cpp
+// Purpose: Implemenation of the Person class.
+///////////////////////////////////////////////////////////////////////////
 #include <string>
-
 #include "person.h"
 
+// Constructors
+Person::Person();
 Person::Person(std::string name, std::string email, std::string phone) : _name(name), _email(email), _phone(phone) {};
 
-
+// Get name
 std::string Person::name() const {
   return _name;
 }
 
+// Set name
 void Person::name(std::string name) 
 {
   this->_name = name;
+}
+
+// Get email
+std::string Person::email() {
+  return _email;
+}
+
+// Set email
+void Person::email(std::string email) {
+  this->_email = email;
+}
+
+// Get phone
+std:string Person::phone() {
+  return _phone;
+}
+
+// Set phone
+void Person::phone(std:string phone) {
+  this->_phone = phone;
 }
--- hw1/person.h
+++ hw1/person.h
@@ -1,33 +1,26 @@
 ///////////////////////////////////////////////////////////////////////////
 // File: person.h
-// Purpose: TBD.
+// Purpose: Class definition of a Person.
 ///////////////////////////////////////////////////////////////////////////
-
-// This will be a pure abstract class from which the concrete
-// classes Employee and Customer will be dervied
-
 #ifndef PERSON_H
 #define PERSON_H
-
 #include <string>
 
 class Person
 {
 public:
-
   // Default constructor
   Person() {};
 
   // Parameterized constructor
   Person(std::string name, std::string email, std::string phone);
 
+  // REMOVE: Method signatures to be implemented in person.cpp file
   std::string name() const;
   void name(std::string _name);
 
-
-  // TODO
-
 private:
+  // REMOVE: Member variables
   std::string _name;
   std::string _email;
   std::string _phone;
