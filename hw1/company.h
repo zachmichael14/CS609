4898b4d2f5e63533824eaf41e1a5565ae540979f
zachmichael14 <zachmichael14@gmail.com>
2023-09-06 12:41:54 -0500

    Add private members

--- hw1/company.h
+++ hw1/company.h
@@ -4,5 +4,23 @@
 ///////////////////////////////////////////////////////////////////////////
 #ifndef COMPANY_H
 #define COMPANY_H
+#include <vector>
+#include "employee.h"
+#include "customer.h"
 
+class Company
+{
+public:
+  Company(std::string name);
+
+  std::string name() const;
+  void add_employee(Employee _employee)
+
+
+
+private:
+  std::vector<Employee>* _employees;
+  std::vector<Customer>* _customers;
+  std::string _name;
+};
 #endif
