6adfffc55a4e1d4cc7aa0c7e64ff2de5a4818780
zachmichael14 <zachmichael14@gmail.com>
2023-09-07 19:44:13 -0500

    Add manage employees menu item def

---
+++ hw1/manage_employees.h
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////
+// File: manage_employees.h
+// Purpose: Class definition of an employee management menu item.
+///////////////////////////////////////////////////////////////////////////
+#ifndef MANAGE_EMPLOYEES_H
+#define MANAGE_EMPLOYEES_H
+#include <string>
+#include <vector>
+#include "menu_item.h"
+#include "employee.h"
+
+class Manage_Employees : public Menu_Item
+{
+public:
+
+  // Constructor
+  Manage_Employees(std::vector<Employee> *_employees);
+
+  // Get text for the menu item
+  virtual std::string label();
+
+  // Function to execute when this item is selected
+  virtual void selected();
+
+private:
+  std::vector<Employee> *_employees;
+};
+#endif
